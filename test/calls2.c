2004e61694fc285c63f308c8ca46da72ea073f41
kinderjosh <kinderjoshua1@gmail.com>
2024-12-12 10:19:02 +0000

    Add (sort of) unlimited params and args

---
+++ test/calls2.c
@@ -0,0 +1,7 @@
+int test(int a, int b, int c, int d, int e, int f, int g, float f1, float f2, float f3, float f4, float f5, float f6, float f7, float f8, float f9, float f10, float f11, float f12, float f13, float f14, float f15, float f16) {
+    return g;
+}
+
+void main() {
+    test(0, 0, 0, 0, 0, 0, 12, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1);
+}
